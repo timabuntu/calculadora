3c7c5b82c7e1cee259c44a283fe11c60a4ac2d3b
thiago lopes de mello <timabuntu@gmail.com>
2022-01-19 11:32:39 -0300

    feat: base layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Calculator from "./components/Calculator";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl">Calculadora</h1>
+    <div className="min-h-screen flex justify-center items-center mx-auto bg-slate-900">
+      <div>
+        <h1 className="flex justify-center text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+          Calculadora
+        </h1>
+        <div className="py-2">
+          <Calculator />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Calculator/index.jsx
@@ -0,0 +1,9 @@
+const Calculator = () => {
+  return (
+    <div className="h-[320px] w-[235px] rounded-md overflow-hidden bg-slate-200">
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Calculator;
